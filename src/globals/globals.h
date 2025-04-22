c5954df13184115bb3142a70c2253403a56e6b58
Timo Reichl <thetredev@gmail.com>
2025-04-22 04:08:24 +0000

    src/globals.h: fix include guard comment
    Signed-off-by: Timo Reichl <thetredev@gmail.com>

--- src/globals/globals.h
+++ src/globals/globals.h
@@ -8,7 +8,7 @@ namespace srcds::tickrate_enabler::globals {
 // Represents the tick interval value calculated for a specific tick rate value.
 extern float cmdline_tick_interval;
 
-} // namespace srcds::tickrate_enabler
+} // namespace srcds::tickrate_enabler::globals
 
 
 #endif // SRCDS_TICKRATE_ENABLER__GLOBALS_H_
